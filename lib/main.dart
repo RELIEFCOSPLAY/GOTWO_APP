ab5b281d85ae178a7f9800b312a74d44e2ae274e
RELIEFCOSPLAY <reliefcosplay@gmail.com>
2024-06-05 09:01:30 +0700

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:gotwo_app/gotwo_Information.dart';
 // ignore: unused_import
 import 'package:gotwo_app/gotwo_Register.dart';
+// ignore: unused_import
 import 'package:gotwo_app/gotwo_Verification.dart';
 // ignore: unused_import
 import 'package:gotwo_app/gotwo_condition.dart';
@@ -26,7 +27,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: const Color(0xff1a1c43)),
         useMaterial3: true,
       ),
-      home: GotwoVerification(),
+      home: GotwoCondition(),
     );
   }
 }
