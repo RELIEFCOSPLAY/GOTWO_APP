46dacbb4779817359c9b515854de05314771a006
S1bu1 <6431501117@lamduan.mfu.ac.th>
2024-06-05 17:21:15 +0700

    update

--- lib/gotwo_request.dart
+++ lib/gotwo_request.dart
@@ -12,6 +12,7 @@ class _HomeState extends State<Home> {
       TextEditingController();
   final TextEditingController _endLocationController = TextEditingController();
   final _formKey = GlobalKey<FormState>();
+
   Future<void> _showDialog() async {
     return showDialog(
       context: context,
@@ -37,17 +38,68 @@ class _HomeState extends State<Home> {
       context: context,
       builder: (BuildContext context) {
         return AlertDialog(
-          title: const Text('Request'),
-          content: const Text(
-              'There is a request to join. Do you still want to delete this post?'),
-          actions: <Widget>[
-            TextButton(
-              onPressed: () {
-                Navigator.of(context).pop();
-              },
-              child: const Text('Yes'),
+          // title: const Text('Request'),
+          content: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              const Text(
+                  'There is a request to join. Do you still want to delete this post?'),
+              const SizedBox(height: 10),
+              // add black textfield
+              TextFormField(
+                decoration: const InputDecoration(
+                  border: OutlineInputBorder(),
+                ),
+              ),
+            ],
+          ),
+          actions: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                ElevatedButton(
+                  onPressed: () async {
+                    Navigator.of(context).pop();
+                  },
+                  style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.red,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(0),
+                      )),
+                  child:
+                      const Text("Yes", style: TextStyle(color: Colors.white)),
+                ),
+                ElevatedButton(
+                  onPressed: () async {
+                    // close dialog
+                    Navigator.of(context).pop();
+                  },
+                  style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.grey,
+                      // set border radius to 0
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(0),
+                      )),
+                  child: const Text("Cancel",
+                      style: TextStyle(color: Colors.white)),
+                ),
+              ],
             ),
           ],
+          // actions: <Widget>[
+          //   TextButton(
+          //     onPressed: () {
+          //       Navigator.of(context).pop();
+          //     },
+          //     child: const Text('Cancel', style: TextStyle(color: Colors.grey)),
+          //   ),
+          //   TextButton(
+          //     onPressed: () {
+          //       Navigator.of(context).pop();
+          //     },
+          //     child: const Text('Yes', style: TextStyle(color: Colors.red)),
+          //   ),
+          // ],
         );
       },
     );
@@ -57,64 +109,66 @@ class _HomeState extends State<Home> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        backgroundColor: const Color(0xFF1C2C5E),
+        elevation: 0,
+        leading: IconButton(
+          icon: const Icon(Icons.arrow_back_ios, color: Colors.white),
+          onPressed: () {},
+        ),
         title: const Text('Request', style: TextStyle(color: Colors.white)),
         centerTitle: true,
-        backgroundColor: Color(0xff1a1c43),
-        leading: IconButton(color: Colors.white,
-          onPressed: () {},
-          icon: IconButton(
-            icon: const Icon(Icons.arrow_back_ios),
-            onPressed: () {},
-            ),
-          ),
       ),
+      // backgroundColor: const Color(0xFF1C2C5E),
       body: Center(
         child: Column(
           children: <Widget>[
+            // add border raidus to the container
             const SizedBox(height: 20),
             const CircleAvatar(
-              child: Icon(Icons.account_circle_outlined, size: 40),
+              radius: 30,
+              child: Icon(Icons.account_circle_outlined, size: 70),
+              backgroundColor: Colors.white,
             ),
             const SizedBox(height: 10),
             const Text("Name Lastname",
-                style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold)),
+                style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
             const SizedBox(height: 10),
             const Row(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
                 Icon(
-                  Icons.transgender,
-                  size: 16,
+                  Icons.face,
+                  size: 20,
                 ),
                 SizedBox(width: 5),
-                Text(": Male", style: TextStyle(fontSize: 16)),
+                Text("Male", style: TextStyle(fontSize: 20)),
               ],
             ),
-            const SizedBox(height: 5),
+            const SizedBox(height: 10),
             const Row(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
                 Icon(
-                  Icons.credit_card_outlined,
-                  size: 16,
+                  Icons.credit_card,
+                  size: 20,
                 ),
                 SizedBox(width: 5),
-                Text("50 THB", style: TextStyle(fontSize: 16))
+                Text("50 THB", style: TextStyle(fontSize: 20)),
               ],
             ),
-            const SizedBox(height: 5),
+            const SizedBox(height: 10),
             const Row(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
                 Icon(
                   Icons.date_range,
-                  size: 16,
+                  size: 20,
                 ),
                 SizedBox(width: 5),
-                Text("Date: 24/03/2024", style: TextStyle(fontSize: 16))
+                Text("Date: 24/03/2024", style: TextStyle(fontSize: 20)),
               ],
             ),
-            const SizedBox(height: 20),
+            const SizedBox(height: 40),
             Container(
               padding: const EdgeInsets.all(16),
               width: MediaQuery.of(context).size.width * 0.8,
@@ -132,95 +186,74 @@ class _HomeState extends State<Home> {
               ),
               child: Form(
                 key: _formKey,
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.center,
+                child: const Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
-                    const Text(
+                    Text(
                       "Pick up",
                       style:
                           TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
                     ),
-                    const SizedBox(height: 10),
-                    const Row(
+                    SizedBox(height: 10),
+                    Row(
                       children: [
                         Icon(
-                          Icons.flag_circle,
+                          Icons.trip_origin,
                         ),
                         SizedBox(width: 10),
                         Expanded(
                           child: Text("Mae Fah Luang(D1)"),
-                          //  TextFormField(
-                          //   controller: _startLocationController,
-                          //   decoration: const InputDecoration(
-                          //     labelText: 'Start Location',
-                          //   ),
-                          //   validator: (value) {
-                          //     if (value == null || value.isEmpty) {
-                          //       return 'Please enter place to pick you up';
-                          //     }
-                          //     return null;
-                          //   },
-                          // ),
                         ),
                       ],
                     ),
-                    const SizedBox(height: 10),
-                    const Row(
+                    SizedBox(height: 10),
+                    Text(
+                      "Drop",
+                      style:
+                          TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+                    ),
+                    SizedBox(height: 10),
+                    Row(
                       children: [
                         Icon(Icons.location_on, color: Colors.red),
                         SizedBox(width: 10),
-                        Expanded(child: Text("Lotus Fah Thai")
-                            // TextFormField(
-                            //   controller: _endLocationController,
-                            //   decoration: const InputDecoration(
-                            //     labelText: 'End Location',
-                            //   ),
-                            //   validator: (value) {
-                            //     if (value == null || value.isEmpty) {
-                            //       return 'Please enter place to go';
-                            //     }
-                            //     return null;
-                            //   },
-                            // ),
-                            ),
-                      ],
-                    ),
-                    const SizedBox(height: 20),
-                    const Text(
-                      "Bring your own a helmet.",
-                      style: TextStyle(fontSize: 14, color: Colors.red),
-                    ),
-                    const SizedBox(height: 20),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        ElevatedButton(
-                          onPressed: () async {
-                            _showDialog();
-                          },
-                          style: ElevatedButton.styleFrom(
-                            backgroundColor: Colors.green,
-                          ),
-                          child: const Text("Accept",
-                              style: TextStyle(color: Colors.white)),
-                        ),
-                        const SizedBox(width: 10),
-                        ElevatedButton(
-                          onPressed: () async {
-                            // Handle the reject logic here
-                            _showRejectDialog();
-                          },
-                          style: ElevatedButton.styleFrom(
-                            backgroundColor: Colors.red,
-                          ),
-                          child: const Text("Reject",
-                              style: TextStyle(color: Colors.white)),
-                        ),
+                        Expanded(child: Text("Lotus Fah Thai")),
                       ],
                     ),
                   ],
                 ),
               ),
+            ),
+            const SizedBox(height: 20),
+            const Text(
+              "Bring your own a helmet.",
+              style: TextStyle(fontSize: 14, color: Colors.red),
+            ),
+            const SizedBox(height: 20),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                ElevatedButton(
+                  onPressed: () async {
+                    _showDialog();
+                  },
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.green,
+                  ),
+                  child: const Text("Accept",
+                      style: TextStyle(color: Colors.white)),
+                ),
+                ElevatedButton(
+                  onPressed: () async {
+                    _showRejectDialog();
+                  },
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.red,
+                  ),
+                  child: const Text("Reject",
+                      style: TextStyle(color: Colors.white)),
+                ),
+              ],
             ),
           ],
         ),
