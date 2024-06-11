3d381fb5a7f3a8006aed9882957422422a04b09f
RELIEFCOSPLAY <reliefcosplay@gmail.com>
2024-06-11 16:23:32 +0700

    main update

--- lib/gotwo_PostPage.dart
+++ lib/gotwo_PostPage.dart
@@ -9,6 +9,32 @@ class GotwoPostpage extends StatefulWidget {
 
 class _GotwoPostpageState extends State<GotwoPostpage> {
   TextEditingController searchController = TextEditingController();
+  List testDate = [
+    {
+      'from': 'hat',
+      'date': '01/12/2077',
+      'time': '08:00',
+      'to': 'K7',
+      'price': 30,
+      'status': 'Paid'
+    },
+    {
+      'from': 'bag',
+      'date': '05/11/2078',
+      'time': '09:27',
+      'to': 'E2',
+      'price': 50,
+      'status': 'Unpaid'
+    },
+    {
+      'from': 'sock',
+      'date': '10/10/2079',
+      'time': '10:30',
+      'to': 'M1',
+      'price': 120,
+      'status': 'Paid'
+    },
+  ];
   @override
   Widget build(Object context) {
     return Container(
@@ -24,6 +50,31 @@ class _GotwoPostpageState extends State<GotwoPostpage> {
       ),
       child: Scaffold(
         backgroundColor: Colors.transparent,
+        appBar: AppBar(
+          backgroundColor: const Color(0xffffffff),
+          title: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    _backButton(),
+                    const Text(
+                      "Post",
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                          fontSize: 30,
+                          color: Color(0xff1a1c43),
+                          fontWeight: FontWeight.bold),
+                    ),
+                    _postButton(),
+                  ],
+                ),
+              ],
+            ),
+          ),
+        ),
         body: _page(),
       ),
     );
@@ -31,21 +82,12 @@ class _GotwoPostpageState extends State<GotwoPostpage> {
 
   Widget _page() {
     return Padding(
-      padding: const EdgeInsets.only(top: 50),
+      padding: const EdgeInsets.only(top: 0.0),
       child: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.start,
           children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              children: [
-                _backButton(),
-                _aText(),
-                _postButton(),
-              ],
-            ),
             _sarchField("Search", searchController),
-            _cardBtn(),
             _cardBtn(),
           ],
         ),
@@ -63,15 +105,6 @@ class _GotwoPostpageState extends State<GotwoPostpage> {
         size: 30,
         color: Color(0xff1a1c43),
       ),
-    );
-  }
-
-  Widget _aText() {
-    return const Text(
-      "Post",
-      textAlign: TextAlign.center,
-      style: TextStyle(
-          fontSize: 30, color: Color(0xff1a1c43), fontWeight: FontWeight.bold),
     );
   }
 
@@ -93,7 +126,7 @@ class _GotwoPostpageState extends State<GotwoPostpage> {
         borderSide: const BorderSide(color: Color(0xff1a1c43)));
 
     return Padding(
-      padding: const EdgeInsets.all(12.0),
+      padding: const EdgeInsets.only(top: 12, left: 12, right: 12, bottom: 12),
       child: SizedBox(
         child: TextField(
           style: const TextStyle(
@@ -111,6 +144,7 @@ class _GotwoPostpageState extends State<GotwoPostpage> {
             suffixIcon: GestureDetector(
               onTap: () {
                 debugPrint("Search : ${searchController.text}");
+                
               },
               child: const Icon(
                 Icons.search,
@@ -124,105 +158,118 @@ class _GotwoPostpageState extends State<GotwoPostpage> {
   }
 
   Widget _cardBtn() {
-    return Padding(
-      padding: const EdgeInsets.all(8.0),
-      child: SizedBox(
-        width: 300,
-        height: 100,
-        child: ElevatedButton(
-          onPressed: () {
-            debugPrint("Card");
-          },
-          style: ButtonStyle(
-              backgroundColor: const WidgetStatePropertyAll(Colors.white),
-              shape: WidgetStateProperty.all<RoundedRectangleBorder>(
-                  RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(18.0),
-                      side: const BorderSide(color: Color(0xff1a1c43))))),
-          child: const SizedBox(
-            width: double.infinity,
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Row(
-                      children: [
-                        Icon(
-                          Icons.location_on,
-                          color: Color(0xff1a1c43),
-                          size: 20.0,
-                        ),
-                        SizedBox(width: 5),
-                        Text(
-                          "From: xxxx",
-                          textAlign: TextAlign.center,
-                          style:
-                              TextStyle(fontSize: 12, color: Color(0xff1a1c43)),
-                        ),
-                      ],
-                    ),
-                    Text(
-                      "Date: 24/03/2024 ",
-                      textAlign: TextAlign.center,
-                      style: TextStyle(fontSize: 12, color: Color(0xff1a1c43)),
-                    ),
-                    Text(
-                      "Time: 10:30 AM",
-                      textAlign: TextAlign.center,
-                      style: TextStyle(fontSize: 12, color: Color(0xff1a1c43)),
-                    ),
-                  ],
+    return SizedBox(
+      width: 320,
+      height: 450,
+      child: ListView.builder(
+        itemCount: testDate.length,
+        itemBuilder: (context, index) {
+          return Padding(
+            padding:
+                const EdgeInsets.only(left: 8, right: 8, top: 4, bottom: 8),
+            child: SizedBox(
+              width: 300,
+              height: 100,
+              child: ElevatedButton(
+                onPressed: () {
+                  debugPrint("CardRequest ${testDate[index]['from']}");
+                },
+                style: ButtonStyle(
+                    backgroundColor:
+                        const WidgetStatePropertyAll(Color(0xfffbf8ff)),
+                    shape: WidgetStateProperty.all<RoundedRectangleBorder>(
+                        RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(18.0),
+                            side: const BorderSide(color: Color(0xff1a1c43))))),
+                child: SizedBox(
+                  width: double.infinity,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Row(
+                            children: [
+                              const Icon(
+                                Icons.location_on,
+                                color: Color(0xff1a1c43),
+                                size: 20.0,
+                              ),
+                              const SizedBox(width: 5),
+                              Text(
+                                "From: ${testDate[index]['from']}",
+                                textAlign: TextAlign.center,
+                                style: const TextStyle(
+                                    fontSize: 12, color: Color(0xff1a1c43)),
+                              ),
+                            ],
+                          ),
+                          Text(
+                            "Date: ${testDate[index]['date']} ",
+                            textAlign: TextAlign.center,
+                            style: const TextStyle(
+                                fontSize: 12, color: Color(0xff1a1c43)),
+                          ),
+                          Text(
+                            "Time: ${testDate[index]['time']}",
+                            textAlign: TextAlign.center,
+                            style: const TextStyle(
+                                fontSize: 12, color: Color(0xff1a1c43)),
+                          ),
+                        ],
+                      ),
+                      const SizedBox(
+                        width: 10,
+                      ),
+                      const Icon(Icons.arrow_forward, color: Color(0xff1a1c43)),
+                      const SizedBox(
+                        width: 10,
+                      ),
+                      Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Row(
+                            children: [
+                              const Icon(
+                                Icons.tour,
+                                color: Color(0xff1a1c43),
+                                size: 20.0,
+                              ),
+                              const SizedBox(width: 5),
+                              Text(
+                                "To: ${testDate[index]['to']}",
+                                textAlign: TextAlign.center,
+                                style: const TextStyle(
+                                    fontSize: 12, color: Color(0xff1a1c43)),
+                              ),
+                            ],
+                          ),
+                          Row(
+                            children: [
+                              Text(
+                                "${testDate[index]['price']} ",
+                                textAlign: TextAlign.center,
+                                style: const TextStyle(
+                                    fontSize: 20, color: Color(0xff1a1c43)),
+                              ),
+                              const Text(
+                                "THB",
+                                textAlign: TextAlign.center,
+                                style: TextStyle(
+                                    fontSize: 10, color: Color(0xff1a1c43)),
+                              ),
+                            ],
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
                 ),
-                SizedBox(
-                  width: 10,
-                ),
-                Icon(Icons.arrow_forward, color: Color(0xff1a1c43)),
-                SizedBox(
-                  width: 10,
-                ),
-                Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Row(
-                      children: [
-                        Icon(
-                          Icons.tour,
-                          color: Color(0xff1a1c43),
-                          size: 20.0,
-                        ),
-                        SizedBox(width: 5),
-                        Text(
-                          "To: xxxx",
-                          textAlign: TextAlign.center,
-                          style:
-                              TextStyle(fontSize: 12, color: Color(0xff1a1c43)),
-                        ),
-                      ],
-                    ),
-                    Row(
-                      children: [
-                        Text(
-                          "50 ",
-                          textAlign: TextAlign.center,
-                          style:
-                              TextStyle(fontSize: 20, color: Color(0xff1a1c43)),
-                        ),
-                        Text(
-                          "THB",
-                          textAlign: TextAlign.center,
-                          style:
-                              TextStyle(fontSize: 10, color: Color(0xff1a1c43)),
-                        ),
-                      ],
-                    ),
-                  ],
-                ),
-              ],
+              ),
             ),
-          ),
-        ),
+          );
+        },
       ),
     );
   }
--- lib/main.dart
+++ lib/main.dart
@@ -37,7 +37,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: const Color(0xff1a1c43)),
         useMaterial3: true,
       ),
-      home:GotwoDashbordrider(),
+      home:GotwoPostpage(),
     );
   }
 }
