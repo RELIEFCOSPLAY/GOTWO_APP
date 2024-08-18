062e74ca432b3b1a1ef1f5469b2ea6acad137e62
S1bu1 <6431501117@lamduan.mfu.ac.th>
2024-08-18 20:30:28 +0700

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -31,7 +31,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: const Color(0xff1a1c43)),
         useMaterial3: true,
       ),
-      home: const SuccessScreen(),
+      home: const GotwoConCus(),
     );
   }
 }
