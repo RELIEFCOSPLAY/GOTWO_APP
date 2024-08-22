b3ee6f7a781bb178d32a7d5a8e98bc0eb0bd33ac
S1bu1 <6431501117@lamduan.mfu.ac.th>
2024-08-22 21:16:14 +0700

    .

---
+++ lib/gotwo_profile.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class GotwoProfile extends StatefulWidget {
+  const GotwoProfile({super.key});
+
+  @override
+  State<GotwoProfile> createState() => _GotwoProfileState();
+}
+
+class _GotwoProfileState extends State<GotwoProfile> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
