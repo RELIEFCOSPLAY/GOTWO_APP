386e56caa88dd5aba12a2f9b0f01da7db1123042
S1bu1 <6431501117@lamduan.mfu.ac.th>
2024-09-13 22:45:36 +0700

    Update

---
+++ lib/tabs/testsuccess.dart
@@ -0,0 +1,196 @@
+import 'package:flutter/material.dart';
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+
+// ignore: must_be_immutable
+class TabSuccess extends StatefulWidget {
+  @override
+  State<TabSuccess> createState() => _TabSuccessState();
+}
+
+class _TabSuccessState extends State<TabSuccess> {
+  List<dynamic> listData = [];
+
+  List<dynamic> filteredList = [];
+
+  // ฟังก์ชันดึงข้อมูลจาก API
+  Future<void> fetchData() async {
+    final String url =
+        "http://192.168.160.1:80/gotwo/status.php"; // URL ของ API
+    try {
+      final response = await http.get(Uri.parse(url), headers: {
+        'Content-Type': 'application/json;charset=utf-8',
+      });
+
+      if (response.statusCode == 200) {
+        setState(() {
+          listData = json.decode(response.body); // แปลง JSON เป็น List
+          filteredList =
+              listData; // เริ่มต้นให้ filteredList มีค่าเท่ากับ listData ทั้งหมด
+        });
+      } else {
+        print("Failed to load data");
+      }
+    } catch (e) {
+      print("Error be Fix: $e");
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    fetchData(); // ดึงข้อมูลเมื่อเริ่มแอป
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        tab_Confirm(),
+      ],
+    );
+  }
+
+  Widget tab_Confirm() {
+    return Padding(
+      padding: const EdgeInsets.only(top: 8),
+      child: SizedBox(
+        width: 320,
+        height: 500,
+        child: ListView.builder(
+          itemCount: listData.length,
+          itemBuilder: (context, index) {
+            final item = listData[index];
+            return Padding(
+              padding:
+                  const EdgeInsets.only(left: 8, right: 8, top: 4, bottom: 8),
+              child: SizedBox(
+                width: 300,
+                height: 100,
+                child: ElevatedButton(
+                  onPressed: () {
+                    debugPrint("CardRequest ${item['pick_up']}");
+                  },
+                  style: ButtonStyle(
+                    backgroundColor:
+                        WidgetStateProperty.all(Color(0xfffbf8ff)),
+                    shape: WidgetStateProperty.all<RoundedRectangleBorder>(
+                      RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(18.0),
+                        side: BorderSide(color: Color(0xff1a1c43)),
+                      ),
+                    ),
+                  ),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Expanded(
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          crossAxisAlignment: CrossAxisAlignment
+                              .start, // Align text to the start
+                          children: [
+                            Row(
+                              children: [
+                                const Icon(
+                                  Icons.location_on,
+                                  color: Color(0xff1a1c43),
+                                  size: 20.0,
+                                ),
+                                const SizedBox(width: 5),
+                                Expanded(
+                                  child: Text(
+                                    "From: ${item['pick_up'] ?? 'Unknown'}", // Check for null values
+                                    overflow: TextOverflow.ellipsis,
+                                    style: const TextStyle(
+                                      fontSize: 12,
+                                      color: Color(0xff1a1c43),
+                                    ),
+                                  ),
+                                ),
+                              ],
+                            ),
+                            Text(
+                              "Date: ${item['date'] ?? 'Unknown'}",
+                              style: const TextStyle(
+                                fontSize: 12,
+                                color: Color(0xff1a1c43),
+                              ),
+                            ),
+                            Text(
+                              "Time: ${item['time'] ?? 'Unknown'}",
+                              style: const TextStyle(
+                                fontSize: 12,
+                                color: Color(0xff1a1c43),
+                              ),
+                            ),
+                            Text(
+                              "${item['status_helmet'] ?? 'Unknown'}",
+                              style: const TextStyle(
+                                fontSize: 12,
+                                color: Color(0xff1a1c43),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                      const SizedBox(width: 10),
+                      const Icon(Icons.arrow_forward, color: Color(0xff1a1c43)),
+                      const SizedBox(width: 10),
+                      Expanded(
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Row(
+                              children: [
+                                const Icon(
+                                  Icons.tour,
+                                  color: Color(0xff1a1c43),
+                                  size: 20.0,
+                                ),
+                                const SizedBox(width: 5),
+                                Expanded(
+                                  child: Text(
+                                    "To: ${item['at_drop'] ?? 'Unknown'}",
+                                    overflow: TextOverflow.ellipsis,
+                                    style: const TextStyle(
+                                      fontSize: 12,
+                                      color: Color(0xff1a1c43),
+                                    ),
+                                  ),
+                                ),
+                              ],
+                            ),
+                            Row(
+                              children: [
+                                Text(
+                                  "${item['price'] ?? 'N/A'} ",
+                                  style: const TextStyle(
+                                    fontSize: 20,
+                                    color: Color(0xff1a1c43),
+                                  ),
+                                ),
+                                const Text(
+                                  "THB",
+                                  style: TextStyle(
+                                    fontSize: 10,
+                                    color: Color(0xff1a1c43),
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            );
+          },
+        ),
+      ),
+    );
+  }
+}
