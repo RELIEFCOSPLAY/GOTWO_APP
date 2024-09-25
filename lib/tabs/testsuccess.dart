9d3194898e4796ddbe68e9a111a71075e56e0b24
S1bu1 <6431501117@lamduan.mfu.ac.th>
2024-09-26 00:07:33 +0700

    Global ip

--- lib/tabs/testsuccess.dart
+++ lib/tabs/testsuccess.dart
@@ -18,7 +18,7 @@ class _TabSuccessState extends State<TabSuccess> {
   String ipUser = "192.168.160.1:80";
   // ฟังก์ชันดึงข้อมูลจาก API
   Future<void> fetchData() async {
-    final String url = "http://10.0.2.2/gotwo/status.php"; // URL ของ API
+    final String url = "http://${Global.ip_8080}/gotwo/status.php"; // URL ของ API
     try {
       final response = await http.get(Uri.parse(url), headers: {
         'Content-Type': 'application/json;charset=utf-8',
