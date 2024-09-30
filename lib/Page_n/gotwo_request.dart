d82a099cb7cf3010b454eb9f7ee8ada842813d5e
RELIEFCOSPLAY <reliefcosplay@gmail.com>
2024-09-30 23:33:44 +0700

    main

--- lib/Page_n/gotwo_request.dart
+++ lib/Page_n/gotwo_request.dart
@@ -91,6 +91,7 @@ class _GotwoRequestState extends State<GotwoRequest> {
     final item = widget.item;
 
     return Scaffold(
+      backgroundColor: const Color(0xFF1A1C43),
       appBar: AppBar(
         backgroundColor: const Color(0xFF1A1C43),
         elevation: 0,
@@ -104,162 +105,171 @@ class _GotwoRequestState extends State<GotwoRequest> {
         centerTitle: true,
       ),
       body: SafeArea(
-        child: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: <Widget>[
-              const SizedBox(height: 20),
-              const CircleAvatar(
-                radius: 30,
-                child: Icon(Icons.account_circle_outlined, size: 70),
-                backgroundColor: Colors.white,
-              ),
-              const SizedBox(height: 10),
-              Text(
-                "${item['rider_name'] ?? 'Unknown'}",
-                style:
-                    const TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-              ),
-              const SizedBox(height: 10),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  const Icon(Icons.face, size: 18),
-                  const SizedBox(width: 5),
-                  Text(
-                    "${item['rider_gender'] ?? 'Unknown'}",
-                    style: const TextStyle(fontSize: 18),
-                  ),
-                ],
-              ),
-              const SizedBox(height: 10),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  const Icon(Icons.credit_card, size: 18),
-                  const SizedBox(width: 5),
-                  Text(
-                    "${item['price'] ?? 'Unknown'} THB",
-                    style: const TextStyle(fontSize: 18),
-                  ),
-                ],
-              ),
-              const SizedBox(height: 10),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  const Icon(Icons.date_range, size: 18),
-                  const SizedBox(width: 5),
-                  Text(
-                    "Date: ${item['date'] ?? 'Unknown'}",
-                    style: const TextStyle(fontSize: 18),
-                  ),
-                ],
-              ),
-              const SizedBox(height: 20),
-              Container(
-                padding:
-                    const EdgeInsets.symmetric(vertical: 12, horizontal: 16),
-                margin: const EdgeInsets.symmetric(
-                    horizontal: 20), // ปรับระยะขอบให้เหมาะสม
-                decoration: BoxDecoration(
-                  color: Colors.white,
-                  borderRadius: BorderRadius.circular(16),
-                  boxShadow: [
-                    BoxShadow(
-                      color: Colors.grey.withOpacity(0.5),
-                      spreadRadius: 2,
-                      blurRadius: 5,
-                      offset: const Offset(0, 3),
-                    ),
-                  ],
+        child: Container(
+          decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.only(
+              topLeft: Radius.circular(35.0), 
+              topRight: Radius.circular(35.0), 
+            ),
+          ),
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: <Widget>[
+                const SizedBox(height: 20),
+                const CircleAvatar(
+                  radius: 30,
+                  child: Icon(Icons.account_circle_outlined, size: 70),
+                  backgroundColor: Colors.white,
                 ),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
+                const SizedBox(height: 10),
+                Text(
+                  "${item['rider_name'] ?? 'Unknown'}",
+                  style: const TextStyle(
+                      fontSize: 18, fontWeight: FontWeight.bold),
+                ),
+                const SizedBox(height: 10),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
                   children: [
-                    const Text(
-                      "Pick up",
-                      style:
-                          TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
-                    ),
-                    const SizedBox(height: 10),
-                    Row(
-                      children: [
-                        Icon(Icons.trip_origin,
-                            color: Colors.green[800], size: 16),
-                        const SizedBox(width: 10),
-                        Expanded(
-                          child: Text(
-                            item['pick_up'] ?? 'Unknown',
-                            style: const TextStyle(fontSize: 14),
-                          ),
-                        ),
-                      ],
-                    ),
-                    const SizedBox(height: 15),
-                    const Text(
-                      "Drop",
-                      style:
-                          TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
-                    ),
-                    const SizedBox(height: 10),
-                    Row(
-                      children: [
-                        const Icon(Icons.location_on,
-                            color: Colors.red, size: 16),
-                        const SizedBox(width: 10),
-                        Expanded(
-                          child: Text(
-                            item['at_drop'] ?? 'Unknown',
-                            style: const TextStyle(fontSize: 14),
-                          ),
-                        ),
-                      ],
+                    const Icon(Icons.face, size: 18),
+                    const SizedBox(width: 5),
+                    Text(
+                      "${item['rider_gender'] ?? 'Unknown'}",
+                      style: const TextStyle(fontSize: 18),
                     ),
                   ],
                 ),
-              ),
-              const SizedBox(height: 10),
-              const Text(
-                "Bring your own a helmet.",
-                style: TextStyle(fontSize: 14, color: Colors.red),
-              ),
-              const SizedBox(height: 10),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  ElevatedButton(
-                    onPressed: () async {
-                      _showDialog();
-                    },
-                    style: ElevatedButton.styleFrom(
-                      backgroundColor: Colors.green,
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(20),
-                      ),
+                const SizedBox(height: 10),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    const Icon(Icons.credit_card, size: 18),
+                    const SizedBox(width: 5),
+                    Text(
+                      "${item['price'] ?? 'Unknown'} THB",
+                      style: const TextStyle(fontSize: 18),
                     ),
-                    child: const Text("Accept",
-                        style: TextStyle(color: Colors.white)),
-                  ),
-                  ElevatedButton(
-                    onPressed: () async {
-                      debugPrint(item.toString());
-                      _showRejectDialog();
-                    },
-                    style: ElevatedButton.styleFrom(
-                      backgroundColor: Colors.red,
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(20),
-                      ),
+                  ],
+                ),
+                const SizedBox(height: 10),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    const Icon(Icons.date_range, size: 18),
+                    const SizedBox(width: 5),
+                    Text(
+                      "Date: ${item['date'] ?? 'Unknown'}",
+                      style: const TextStyle(fontSize: 18),
                     ),
-                    child: const Text("Reject",
-                        style: TextStyle(color: Colors.white)),
+                  ],
+                ),
+                const SizedBox(height: 20),
+                Container(
+                  padding:
+                      const EdgeInsets.symmetric(vertical: 12, horizontal: 16),
+                  margin: const EdgeInsets.symmetric(
+                      horizontal: 20), // ปรับระยะขอบให้เหมาะสม
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(16),
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.grey.withOpacity(0.5),
+                        spreadRadius: 2,
+                        blurRadius: 5,
+                        offset: const Offset(0, 3),
+                      ),
+                    ],
                   ),
-                ],
-              ),
-              const SizedBox(height: 10),
-            ],
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      const Text(
+                        "Pick up",
+                        style: TextStyle(
+                            fontSize: 16, fontWeight: FontWeight.bold),
+                      ),
+                      const SizedBox(height: 10),
+                      Row(
+                        children: [
+                          Icon(Icons.trip_origin,
+                              color: Colors.green[800], size: 16),
+                          const SizedBox(width: 10),
+                          Expanded(
+                            child: Text(
+                              item['pick_up'] ?? 'Unknown',
+                              style: const TextStyle(fontSize: 14),
+                            ),
+                          ),
+                        ],
+                      ),
+                      const SizedBox(height: 15),
+                      const Text(
+                        "Drop",
+                        style: TextStyle(
+                            fontSize: 16, fontWeight: FontWeight.bold),
+                      ),
+                      const SizedBox(height: 10),
+                      Row(
+                        children: [
+                          const Icon(Icons.location_on,
+                              color: Colors.red, size: 16),
+                          const SizedBox(width: 10),
+                          Expanded(
+                            child: Text(
+                              item['at_drop'] ?? 'Unknown',
+                              style: const TextStyle(fontSize: 14),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+                const SizedBox(height: 10),
+                const Text(
+                  "Bring your own a helmet.",
+                  style: TextStyle(fontSize: 14, color: Colors.red),
+                ),
+                const SizedBox(height: 10),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    ElevatedButton(
+                      onPressed: () async {
+                        _showDialog();
+                      },
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: Colors.green,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(20),
+                        ),
+                      ),
+                      child: const Text("Accept",
+                          style: TextStyle(color: Colors.white)),
+                    ),
+                    ElevatedButton(
+                      onPressed: () async {
+                        debugPrint(item.toString());
+                        _showRejectDialog();
+                      },
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: Colors.red,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(20),
+                        ),
+                      ),
+                      child: const Text("Reject",
+                          style: TextStyle(color: Colors.white)),
+                    ),
+                  ],
+                ),
+                const SizedBox(height: 10),
+              ],
+            ),
           ),
         ),
       ),
